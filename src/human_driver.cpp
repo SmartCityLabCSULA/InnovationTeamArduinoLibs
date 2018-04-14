3e71c8b173561f8e44a246ac8a48cac8149ce115
jay ess <mentoce@gmail.com>
2018-04-14 01:44:47 -0700

    Add implementation of HumanDriver class

---
+++ src/human_driver.cpp
@@ -0,0 +1,38 @@
+#include <human_driver.h>
+
+namespace control
+{
+double HumanDriver::h_dot(double vel_1)
+{
+    // vel_1 (float): velocity of vehicle in front of this vehicle
+    return hdot = vel_1 - current_velocity_;
+}
+
+double HumanDriver::v_dot(double vel_1, double headway)
+{
+    // vel_1 (float): velocity of vehicle in front of this vehicle
+    // headway (float): headway between this vehicle and next
+    vdot = alpha*(velocity(headway) - current_velocity_);
+    return vdot + beta * (vel_1 - current_velocity_);
+}
+
+double HumanDriver::velocity(double h)
+{
+    return range_policy_.velocity(h);
+}
+
+double HumanDriver::range_policy(double h)
+{
+    return range_policy_.velocity(h);
+}
+
+double HumanDriver::get_velocity()
+{
+    return current_velocity_;
+}
+
+void HumanDriver::set_velocity(double v)
+{
+    current_velocity_ = v;
+}
+}
