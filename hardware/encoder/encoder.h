599f144c30fc23da4865fdda6c08d9589846d405
jay ess <mentoce@gmail.com>
2018-04-15 23:53:38 -0700

    Move code into separate libs

---
+++ hardware/encoder/encoder.h
@@ -0,0 +1,11 @@
+#ifndef HW_ENCODER_H
+#define HW_ENCODER_H
+
+class Encoder
+{
+  public:
+    Encoder();
+    double ticks_to_velocity(double, double, int, int);
+
+};
+#endif // HW_ENCODER_H
---
+++ hardware/schmitt_trigger/schmitt_trigger.h
@@ -0,0 +1,8 @@
+#ifndef HW_SCHMITT_TRIGGER_H
+#define HW_SCHMITT_TRIGGER_H
+
+class SchmittTrigger{
+    SchmittTrigger();
+    int count_pulses(int[], int);
+};
+#endif // HW_SCHMITT_TRIGGER_H
